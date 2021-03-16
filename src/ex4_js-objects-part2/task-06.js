b1f7c9f6d249607c47884a8df7087164b9dca06b
Daria <scolotina@gmail.com>
2021-03-16 23:19:54 +0300

    6 task

---
+++ src/ex4_js-objects-part2/task-06.js
@@ -0,0 +1,6 @@
+function toUpStr(str){
+    let newStr = str.split(" ").map((element) => 
+    element[0].toUpperCase() + element.slice(1));
+    return newStr.join(' ');
+}
+module.exports = toUpStr;
