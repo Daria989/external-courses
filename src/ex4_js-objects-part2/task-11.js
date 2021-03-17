a074fee7144a1aad19438825d72d78c05ef13aff
Daria <scolotina@gmail.com>
2021-03-18 00:16:06 +0300

    11 task

---
+++ src/ex4_js-objects-part2/task-11.js
@@ -0,0 +1,13 @@
+function numOfEl(string) {
+    let sum = {};
+    string.split('').map((element) => {
+        sum[element] = !sum[element] ? 1 : sum[element] + 1;
+	});
+    for (let key in sum) {
+        if(sum.hasOwnProperty(key)) {
+            console.log(key + ": " + sum[key]);
+        }
+    }
+    return;
+}
+module.exports = numOfEl;
