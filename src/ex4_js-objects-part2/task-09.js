f4047cb399ad2f0df0fe767e16003b0344e19d10
Daria <scolotina@gmail.com>
2021-03-17 14:05:57 +0300

    9 task

---
+++ src/ex4_js-objects-part2/task-09.js
@@ -0,0 +1,6 @@
+function strToStr(strMain, str, num) {
+    let newStr = strMain.split(" ");
+    let strDel = newStr.splice(num + 1, 0, str);
+    return newStr.join(" ");
+}
+module.exports = strToStr;
