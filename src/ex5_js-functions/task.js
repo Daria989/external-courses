a0be74f8cfd9191e4d16a0511bbadbe5bfec14fb
Daria <scolotina@gmail.com>
2021-03-23 15:23:01 +0300

    Solution of task 'Calculator'

--- src/ex5_js-functions/task.js
+++ src/ex5_js-functions/task.js
@@ -1,36 +1,38 @@
 function Calc() {
-    accumulator = 0;
+    let accumulator = 0;
     
     return {
-        getResult:  () => {
-                        return accumulator;
-                    },
+        getResult: function getRes() {
+                       return accumulator;
+                   },
 
-        reset:      () => {
-                        accumulator = 0; 
-                    },
+        reset: function res() {
+                   accumulator = 0; 
+               },
 
-        add:        function add(a = 0) {
-                        accumulator += a;
-                        return add;
-                    },
+        add: function add(a = 0) { 
+                 accumulator += a;
+                 return add;
+             },
 
-        subtract:   function sub(a = 0) {
-                        accumulator -= a;
-                        return sub;
-                    },
+        subtract: function sub(a = 0) {
+                      accumulator -= a;
+                      return sub;
+                  },
 
-        multiply:   function mul(a = 0) {
-                        accumulator *= a;
-                        return mul;
-                    },
+        multiply: function mul(a = 1) {
+                      accumulator *= a;
+                      return mul;
+                  },
 
-        divide:     function div(a = 1) {
-                        accumulator /= a;
-                        return div;
+        divide: function div(a = 1) {
+                    if (a === 0 || a === Infinity || isNaN(a)) {
+                        return console.log("Result is unknown");
                     }
-        }
-
+                    accumulator /= a;
+                    return div;
+                },
+    }
 }
 
 let Calculator = Calc();
