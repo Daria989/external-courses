83f054a2d2229bd48ae8522f6b94023659212041
Daria <scolotina@gmail.com>
2021-03-25 02:11:03 +0300

    Solution of 2 task

---
+++ src/ex6_js-array-methods/task-02.js
@@ -0,0 +1,10 @@
+function someOfElements(array, funcCallback) {
+    for (let i = 0; i < array.length; i++) {
+        if (funcCallback(array[i], i, array)) {
+           return true;
+       }
+    }
+    return false;
+}
+
+module.exports = someOfElements;
