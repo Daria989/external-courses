b5727732b5cee9bb6cb13be0ebfb86c9a3c2721c
Daria <scolotina@gmail.com>
2021-03-25 22:10:19 +0300

    Solution of 4 task

---
+++ src/ex6_js-array-methods/task-04.js
@@ -0,0 +1,11 @@
+function filterElements(array, funcCallback) {
+    let newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        if (funcCallback(array[i], i, array)) {
+           newArray.push(array[i]);
+       }
+    }
+    return newArray;
+}
+
+module.exports = filterElements;
