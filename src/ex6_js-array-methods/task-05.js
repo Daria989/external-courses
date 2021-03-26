262bfbca9bc2d2413eda4a0520f20cbeda58e114
Daria <scolotina@gmail.com>
2021-03-26 23:40:43 +0300

    Solution of 5 task

--- src/ex6_js-array-methods/task-05.js
+++ src/ex6_js-array-methods/task-05.js
@@ -1,8 +1,9 @@
 function mapElements(array, funcCallback) {
     let newArray = [];
     for (let i = 0; i < array.length; i++) {
-           newArray.push(funcCallback(array[i], i, array));
-       }
+        newArray.push(funcCallback(array[i], i, array));
+    }
+    
     return newArray;
 }
 
