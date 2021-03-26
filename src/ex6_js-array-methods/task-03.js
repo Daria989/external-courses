db77297a23032a462978e8dde264d03f5e4f068f
Daria <scolotina@gmail.com>
2021-03-26 23:44:19 +0300

    Solution of 3 task

--- src/ex6_js-array-methods/task-03.js
+++ src/ex6_js-array-methods/task-03.js
@@ -2,8 +2,9 @@ function everyElements(array, funcCallback) {
     for (let i = 0; i < array.length; i++) {
         if (!funcCallback(array[i], i, array)) {
            return false;
-       }
+        }
     }
+
     return true;
 }
 
