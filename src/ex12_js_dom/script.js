8268895c0f7c7ce4e22f7d372b050d3b6baa7923
Daria <scolotina@gmail.com>
2021-04-28 15:49:05 +0300

    Code fixing

--- src/ex12_js_dom/script.js
+++ src/ex12_js_dom/script.js
@@ -1,31 +1,32 @@
 let num = 0;
-let arr = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"];
+let arr = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11'];
 let image = document.querySelector('#new-image');
-let buttonLeft = document.querySelector(".button-left");
-let buttonRight = document.querySelector(".button-right");
-buttonRight.onclick = function changeRight() {
+let buttonLeft = document.querySelector('#button-left');
+let buttonRight = document.querySelector('#button-right');
+
+buttonRight.addEventListener("click", changeRight);
+buttonLeft.addEventListener("click", changeLeft);
+
+function changeRight() {
+    
     num++;
     if (num === arr.length) {
         num = 0;
     }
 
-    image.setAttribute("src", 'asset/'+arr[num]+'.jpg');
-    image.setAttribute("class", "animation");
-    setTimeout(function(){
-        image.removeAttribute("class");
-    }, 600);
+    image.setAttribute('src', 'asset/'+arr[num]+'.jpg');
+    image.classList.remove('animation');
+    image.classList.add('animation');
 }
 
-buttonLeft.onclick = function changeLeft() {
-    num--;
+function changeLeft() { 
+    --num;
     if (num === -1) {
-        num = arr.length -1; 
+        num = arr.length - 1; 
     } 
 
-    image.setAttribute("src", 'asset/'+arr[num]+'.jpg'); 
-    image.setAttribute("class", "animation");
-    setTimeout(function(){
-        image.removeAttribute("class");
-    }, 600);
+    image.setAttribute('src', 'asset/'+arr[num]+'.jpg');
+    image.classList.remove('animation');
+    image.classList.add('animation');
 }
 
