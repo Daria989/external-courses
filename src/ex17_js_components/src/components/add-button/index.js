7a2ca90b5034cd8408337ef2a778e16f3a87a072
Daria <scolotina@gmail.com>
2021-06-01 01:44:59 +0300

    ex18_more

---
+++ src/ex17_js_components/src/components/add-button/index.js
@@ -0,0 +1,2 @@
+export {addCardFirst, addCardButton} from './add-button-first.js';
+export {addCard} from './add-button.js';
--- src/ex17_js_components/src/main.js
+++ src/ex17_js_components/src/main.js
@@ -1,25 +1,27 @@
 import {addTemplate, arrowUp} from './components/user-menu/index.js';
-import {addCardButtonBacklog, addCardBacklog, addCardReady, addCardInProgress, addCardFinished} from './components/task-cards/index.js';
+import {createNewList} from './components/create-list/index.js';
+import {addCard} from './components/add-button/index.js';
+import {addCardFirst, addCardButton} from './components/add-button/index.js';
+import {contextMenuButton, contextMenu} from './components/context-menu/index.js';
 
 const avatarButton = document.querySelector('.user-avatar');
 const arrowButton = document.querySelector('.arrow-down');
+const createNewListButton = document.querySelector('.create-list');
 
-const addCardButtonInProgress = document.querySelector('.add-card-button-in-progress');
-addCardButtonInProgress.disabled = true;
-
-const addCardButtonFinished = document.querySelector('.add-card-button-finished');
-addCardButtonFinished.disabled = true;
-
-const addCardButtonReady = document.querySelector('.add-card-button-ready');
-addCardButtonReady.disabled = true;
-
-addCardButtonBacklog.addEventListener('click', addCardBacklog);
-addCardButtonReady.addEventListener('click', addCardReady);
-addCardButtonInProgress.addEventListener('click', addCardInProgress);
-addCardButtonFinished.addEventListener('click', addCardFinished);
+addCardButton[0].addEventListener('click', addCardFirst);
+addCardButton[1].addEventListener('click', addCard);
+addCardButton[2].addEventListener('click', addCard);
+addCardButton[3].addEventListener('click', addCard);
 
 arrowButton.addEventListener('click', addTemplate);
 avatarButton.addEventListener('click', addTemplate);
 
 arrowButton.addEventListener('click', arrowUp);
 avatarButton.addEventListener('click', arrowUp);
+
+createNewListButton.addEventListener('click', createNewList);
+
+contextMenuButton[0].addEventListener('click', contextMenu);
+contextMenuButton[1].addEventListener('click', contextMenu);
+contextMenuButton[2].addEventListener('click', contextMenu);
+contextMenuButton[3].addEventListener('click', contextMenu);
